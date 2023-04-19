3f6dbb4ca50e7982cfa259db03eaa5fbddfe7a8e
evan9633 <evan9633@gmail.com>
2023-04-19 11:02:37 -0500

    shit

--- lab10/Lab10/movement.c
+++ lab10/Lab10/movement.c
@@ -10,14 +10,14 @@
 #include "movement.h"
 #include "open_interface.h"
 
-#define TAPEVALUE 470 //STILL NEED TAPE VALUE
+#define TAPEVALUE 2000 //STILL NEED TAPE VALUE
     oi_t* sensor;
 
     void movement_init(oi_t* s) {
         sensor = s;
     }
 
-    void move_forward(double distance_num){
+    void move_forward(oi_t *sensor,double distance_num){
         int bot_distance = 0;
         oi_setWheels(50,50);
 
@@ -30,7 +30,7 @@
     }
 
 
-        void turn_right(double degrees){
+        void turn_right(oi_t *sensor,double degrees){
             double bot_angle = 0;
             oi_setWheels(-50,50);
 
@@ -41,7 +41,7 @@
             oi_setWheels(0,0);
         }
 
-        void turn_left(double degrees){
+        void turn_left(oi_t *sensor,double degrees){
         double bot_angle = 0;
         oi_setWheels(50,-50);
         while(check_Interrupts() == 0 && bot_angle > degrees * 1) {//STILL NEED TO CALIBRATE
@@ -52,7 +52,7 @@
         oi_setWheels(0,0);
         }
 
-        void move_backward(double distance_num){
+        void move_backward(oi_t *sensor,double distance_num){
             int bot_distance = 0;
             oi_setWheels(-50,-50);
             while(check_Interrupts() == 0 && bot_distance > distance_num * -1) {//STILL NEED TO CALIBRATE
@@ -69,27 +69,27 @@
 
             if (sensor -> wheelDropLeft || sensor -> cliffFrontLeft || sensor -> cliffLeft || sensor -> bumpLeft) {
 
-                move_backward(10);
+                move_backward(sensor,10);
                 return 1;
             } else if (sensor -> wheelDropRight || sensor -> cliffFrontRight || sensor -> cliffRight || sensor -> bumpRight) {
 
-                move_backward(10);
+                move_backward(sensor, 10);
                 return 2;
             } else if (sensor -> cliffLeftSignal > TAPEVALUE) {
 
-                move_backward(10);
+                move_backward(sensor,10);
                 return 3;
             } else if (sensor -> cliffFrontLeftSignal > TAPEVALUE) {
 
-                move_backward(10);
+                move_backward(sensor,10);
                 return 4;
             } else if (sensor -> cliffFrontRightSignal > TAPEVALUE) {
 
-                move_backward(10);
+                move_backward(sensor,10);
                 return 5;
             } else if (sensor -> cliffRightSignal > TAPEVALUE) {
 
-                move_backward(10);
+                move_backward(sensor,10);
                 return 6;
             }
 
