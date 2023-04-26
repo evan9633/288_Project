76a27c027c23f8e599f1dae88b1aaf388529f6d9
evan9633 <evan9633@gmail.com>
2023-04-26 11:42:39 -0500

    fixed things

--- basic.py
+++ basic.py
@@ -19,13 +19,20 @@ class App:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(('192.168.1.1', 288))
 
+        ax.set_ylim([-10,10]) # set y-axis limits
+        ax.set_xlim([-10,10]) # set x-axis limits
+        
         self.receive_thread = threading.Thread(target=self.receive_data)
         self.receive_thread.start()
+
+        self.fig, self.ax = plt.subplots(subplot_kw={'projection': 'polar'})
 
         self.data_obj = None
         self.width = None
         self.angle = None
         self.dist = None
+        self.drop = None
+        self.bump = None
 
     def receive_data(self):
         while True:
@@ -54,10 +61,29 @@ class App:
                         self.angle = float(value)
                     elif key == 'dist':
                         self.dist = float(value)
+                    elif key == 'drop':
+                        self.drop = True
+                    elif key == 'bump':
+                        self.drop = str(value)
 
             # check if all data members have been received and process them
-            if self.width is not None and self.angle is not None and self.dist is not None:
+            if 'dist' in locals() and 'angle' in locals() and dist is not None and angle is not None:
                 # create a new object with the data members
+                if self.ax.lines:
+                    self.ax.lines[-1].set_alpha(0.5)
+
+                if tape is not None:
+                    color = 'r'
+                else:
+                    color = 'g'
+                
+                x = dist * np.cos(np.radians(angle))
+                y = dist * np.sin(np.radians(angle))
+
+                self.ax.plot([angle, angle], [0, dist], linewidth=width, color=color, alpha=1.0)
+                self.ax.plot([angle], [dist], marker='o', markersize=5, color=color, alpha=1.0)
+
+                plt.draw()
                 self.data_obj = {'width': self.width, 'angle': self.angle, 'dist': self.dist}
                 # do something with the data object, such as store it in a list or pass it to another function
                 print(self.data_obj)
@@ -65,6 +91,8 @@ class App:
                 self.width = None
                 self.angle = None
                 self.dist = None
+                self.drop = None
+                self.drop = None
 
     def send(self, event):
         key = event.char
