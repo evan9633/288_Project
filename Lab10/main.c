58217db56fe7e87909a183f81c9f5e3040b1dfd9
Evan <evan9633@gmail.com>
2023-04-30 11:40:16 -0500

    This is the updated scan from Brock

--- Lab10/main.c
+++ Lab10/main.c
@@ -1,18 +1,21 @@
-/**
- * @file lab9_template.c
- * @author
- * Template file for CprE 288 Lab 9
- */
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "Timer.h"
 #include "lcd.h"
 #include "ping_template.h"
-#include "adc.h"
 #include "driverlib/interrupt.h"
-#include "open_interface.h"
-#include "movement.h"
-#include <stdint.h>
+#include "servo.h"
 #include "uart-interrupt.h"
+#include "open_interface.h"
+#include "adc.h"
+#include "movement.h"
+
+#include "structs.h"
+
+#define IRVALUE 400
 
 // Uncomment or add any include directives that are needed
 
@@ -20,137 +23,281 @@
  * MTG18-SLIDES-GPTimerIC.pdf are super awesome and swag
  */
 
-#warning "Possible unimplemented functions"
 #define REPLACEME 0
 
 int main(void) {
+
+
     timer_init(); // Must be called before lcd_init(), which uses timer functions
     lcd_init();
 
-    ping_init();
-    adc_init();
     uart_interrupt_init();
+
     oi_t *sensor_data = oi_alloc(); // do this only once at start of main()
     oi_init(sensor_data); // do this only once at start of main()
+    movement_init(sensor_data);
 
-    while(1){
-        oi_update(sensor_data);
-        char temp = '}';
+    ping_init();
+    servo_init();
+    adc_init();
+
+    while (1) {
+        char g = '/0';
 
 
+//        uart_sendStr("Test");
+        g = uart_receive();
+        if (g == 'w')
+        {
+            move_forward(10);
+        }
+        else if (g == 'a')
+        {
+            turn_left(15);
+        }
+        else if (g == 's')
+        {
+            move_backward(10);
+        }
+        else if (g == 'd')
+        {
+            turn_right(15);
+        }
+
+        if (g == '8') {//0-180
+            int i;
+                int count;
+                int object_counter = 0;
+                uint16_t val;
+                        char str[] = "";
+                                    object objects[10];
+                                    const int distance_threshold = 30;
+
+                                    int same_distance = 0;
+                                    int distance = 0;
+
+                                    cyBOT_Scan_t info[181] = { };
+
+                                    i = 0;
+                                    servo_move(i);
+
+                                    info[0].ir = adc_read();
+                                    info[0].distance = ping_getDistance();
+                                    info[0].angle = i;
+
+                                    int previous_ir = info[0].ir;
+
+                                    i += 1;
+
+                                    while (i <= 180)
+                                    {
+                                        timer_waitMillis(200);
+                                        servo_move(i);
+
+                                        info[i].ir = adc_read();
+                                        info[i].distance = ping_getDistance();
+                                        info[i].angle = i;
+
+                                        double period = START_TIME - END_TIME;
+                                        count += 1;
+
+                                        val = 195.52 - (27 * pow((double) val, 0.246));
 
 
-//        while (temp == '}'){
-//        temp = uart_receive();
+                                        distance = info[i].ir;
+
+                                        if (distance > IRVALUE)
+                                        {
+                                            if (distance <= previous_ir + distance_threshold
+                                                    && distance >= previous_ir - distance_threshold)
+                                            {
+                                                same_distance += 1;
+
+                                            }
+                                            else
+                                            {
+                                                if (same_distance > 2)
+                                                {
+                                                    // sprintf(line, "%d %-10s\n\r", i - 1, "+ object");
+                                                    // uart_sendStr(line);
+
+                                                    // sprintf(line, "%-10d %-10s\n\r", same_distance, "degree [width]");
+                                                    // uart_sendStr(line);
+
+                                                    objects[object_counter].degree =
+                                                            i - (same_distance / 2);
+                                                    objects[object_counter].distance =
+                                                            info[i - (same_distance / 2)].distance;
+                                                    objects[object_counter].width = (same_distance);
+                                                    object_counter++;
+
+                                                }
+                                                same_distance = 0;
+
+                                            }
+
+                                        }
+                                        else
+                                        {
+                                            if (same_distance > 2)
+                                            {
+                                                // sprintf(line, "%d %-10s\n\r", i - (same_distance / 2), "[+] object");
+                                                //uart_sendStr(line);
+
+                                                // sprintf(line, "%-10d %-10s\n\r", same_distance, "[+] degree [width]");
+                                                // uart_sendStr(line);
+
+                                                objects[object_counter].degree = i
+                                                        - (same_distance / 2);
+                                                objects[object_counter].distance = info[i
+                                                        - (same_distance / 2)].distance;
+                                                objects[object_counter].width =
+                                                        (objects[object_counter].distance
+                                                                * tan(same_distance * 3.14 / 180)); //sin(0.0174533 * same_distance) * objects[object_counter].distance;
+
+                                                object_counter++;
+
+                                            }
+                                            same_distance = 0;
+
+                                        }
+                                        previous_ir = distance;
+
+                                        i += 1;
+                                    }
+                                    int z;
+                                    for (z = 0; z < object_counter; z++)
+                                    {
+                                        if (objects[z].distance < 100 && objects[z].degree < 180 && objects[z].width < 90 && objects[z].distance > 0)
+                                        {
+                                            sprintf(str, "width:%d;angle:%d;dist:%d;",
+                                                    objects[z].width, objects[z].degree,
+                                                    objects[z].distance);
+                                            uart_sendStr(str);
+                                            printf(str);
+                                            printf("\n");
+                                        }
+                                    }
+        }
+        if (g == '5') { //60-120
+            int i;
+                int count;
+                int object_counter = 0;
+                uint16_t val;
+                        char str[] = "";
+                                    object objects[10];
+                                    const int distance_threshold = 30;
+
+                                    int same_distance = 0;
+                                    int distance = 0;
+
+                                    cyBOT_Scan_t info[181] = { };
+
+                                    i = 60;
+                                    servo_move(i);
+
+                                    info[0].ir = adc_read();
+                                    info[0].distance = ping_getDistance();
+                                    info[0].angle = i;
+
+                                    int previous_ir = info[0].ir;
+
+                                    i += 1;
+
+                                    while (i <= 120)
+                                    {
+                                        timer_waitMillis(200);
+                                        servo_move(i);
+
+                                        info[i].ir = adc_read();
+                                        info[i].distance = ping_getDistance();
+                                        info[i].angle = i;
+
+                                        double period = START_TIME - END_TIME;
+                                        count += 1;
+
+                                        val = 195.52 - (27 * pow((double) val, 0.246));
+
+
+                                        distance = info[i].ir;
+
+                                        if (distance > IRVALUE)
+                                        {
+                                            if (distance <= previous_ir + distance_threshold
+                                                    && distance >= previous_ir - distance_threshold)
+                                            {
+                                                same_distance += 1;
+
+                                            }
+                                            else
+                                            {
+                                                if (same_distance > 2)
+                                                {
+
+
+                                                    objects[object_counter].degree =
+                                                            i - (same_distance / 2);
+                                                    objects[object_counter].distance =
+                                                            info[i - (same_distance / 2)].distance;
+                                                    objects[object_counter].width = (same_distance);
+                                                    object_counter++;
+
+                                                }
+                                                same_distance = 0;
+
+                                            }
+
+                                        }
+                                        else
+                                        {
+                                            if (same_distance > 2)
+                                            {
+                                                // sprintf(line, "%d %-10s\n\r", i - (same_distance / 2), "[+] object");
+                                                //uart_sendStr(line);
+
+                                                // sprintf(line, "%-10d %-10s\n\r", same_distance, "[+] degree [width]");
+                                                // uart_sendStr(line);
+
+                                                objects[object_counter].degree = i
+                                                        - (same_distance / 2);
+                                                objects[object_counter].distance = info[i
+                                                        - (same_distance / 2)].distance;
+                                                objects[object_counter].width =
+                                                        (objects[object_counter].distance
+                                                                * tan(same_distance * 3.14 / 180)); //sin(0.0174533 * same_distance) * objects[object_counter].distance;
+
+                                                object_counter++;
+
+                                            }
+                                            same_distance = 0;
+
+                                        }
+                                        previous_ir = distance;
+
+                                        i += 1;
+                                    }
+            int z;
+                                    for (z = 0; z < object_counter; z++)
+                                    {
+                                        if (objects[z].distance < 100 && objects[z].degree < 180 && objects[z].width < 90 && objects[z].distance > 0)
+                                        {
+                                            sprintf(str, "width:%d;angle:%d;dist:%d;",
+                                                    objects[z].width, objects[z].degree,
+                                                    objects[z].distance);
+                                            uart_sendStr(str);
+                                            printf(str);
+                                            printf("\n");
+                                        }
+                                    }
+        }
+//        if (g == '6') { //0-90
+//         scan (0,90);
 //        }
-        if (command_flag == 1)
-                {
-
-        if(command_byte == 'q'){
-            move_forward(sensor_data, 2);
-        }
-        if(command_byte == 'w'){
-            move_forward(sensor_data, 5);
-        }
-        if(command_byte == 'e'){
-            move_forward(sensor_data, 7);
-        }
-        if(command_byte == 'r'){
-            move_forward(sensor_data, 10);
-        }
-        if(command_byte == 't'){
-            move_forward(sensor_data, 15);
-        }
-        if(command_byte == 'y'){
-            move_forward(sensor_data, 20);
-        }
-        if(command_byte == 'u'){
-            move_forward(sensor_data, 25);
-        }
-        if(command_byte == 'i'){
-            move_forward(sensor_data, 30);
-        }
-        if(command_byte == 'o'){
-            move_forward(sensor_data, 35);
-        }
-        if(command_byte == 'p'){
-            //scan
-        }
-        // move backwards
-        if(command_byte == 'a'){
-            move_backward(sensor_data, 2);
-               }
-               if(command_byte == 's'){
-                   move_backward(sensor_data, 5);
-               }
-               if(command_byte == 'd'){
-                   move_backward(sensor_data, 7);
-               }
-               if(command_byte == 'f'){
-                   move_backward(sensor_data, 10);
-               }
-               if(command_byte == 'g'){
-                   move_backward(sensor_data, 15);
-               }
-               if(command_byte == 'h'){
-                   move_backward(sensor_data, 20);
-               }
-               if(command_byte == 'j'){
-                   move_backward(sensor_data, 25);
-               }
-               if(command_byte == 'k'){
-                   move_backward(sensor_data, 30);
-               }
-               if(command_byte == 'l'){
-                   move_backward(sensor_data, 35);
-               }
-        // turn right
-        if(command_byte == 'z'){
-            turn_right(sensor_data, 15);
-        }
-        if(command_byte == 'x'){
-            turn_right(sensor_data, 45);
-        }
-        if(command_byte == 'c'){
-            turn_right(sensor_data, 60);
-        }
-        if(command_byte == 'v'){
-            turn_right(sensor_data, 90);
-        }
-        //turn left
-        if(command_byte == 'b'){
-            turn_left(sensor_data, 15);
-        }
-        if(command_byte == 'n'){
-            turn_left(sensor_data, 45);
-        }
-        if(command_byte == 'm'){
-            turn_left(sensor_data, 60);
-        }
-        if(command_byte == ','){
-            turn_left(sensor_data, 90);
-        }
-
-
-
-
-
-        oi_update(sensor_data);
-        //does the api/ui need the tape sensor data or does the bot just need to know that
-        char x[20];
-        //sensor data    bumpleft bumpright tapefrontleft tapefrontright tapeleft taperight clifffrontleft clifffrontright cliffleft cliffright
-
-        //send obj  can't find where objects stored
-        //uart_sendStr("dist: %lf angle: %d width: %d;",  );
-//        int i = 0;
-//        char send_obj[50];
-//        for(i i <= obj[].length; i++){
-//            send_obj += obj[i].distance;
-//            send_obj += obj[i].width;
-//            send_obj += obj[i].angle;
+//        if (g == '4') { //90-180
+//            scan(90,180);
+//
 //        }
-//        uart_sendStr(send_obj);
-        temp = '}';
-         }
+
     }
+
 }
